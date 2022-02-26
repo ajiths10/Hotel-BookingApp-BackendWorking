2cf02655f1d87e0c9ec450467db69447abcc9c6b
Nuclear One <ajithsnair10@gmail.com>
2022-02-26 13:20:55 +0530

    A Hotel booking wesite

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+//Testing purpose
+//Ajith-s
+
+This is a testing site to understand the working of localStorage. This website include
+features like adding new users and remove existing user and edit existing user.
+
+//Testing purpose.
---
+++ main.js
@@ -0,0 +1,87 @@
+// sessionStorage.setItem('name','bob')
+// localStorage.setItem('name','kyle')
+
+// console.log(sessionStorage.getItem('name'))
+
+// document.cookie='name=bob';
+// console.log(document.cookie);
+
+
+let btn=document.querySelector('.btn')
+let namebox=document.querySelector('#name')
+let emailbox=document.querySelector('#email')
+let i=0;
+
+//scan all existing element from localStorage 
+window.addEventListener("DOMContentLoaded",() => {
+    console.log("Testing...");
+    const localStorageobj=localStorage;
+    const localStoragekeys= Object.keys(localStorageobj);
+
+    for(var i=0;i<localStoragekeys.length;i++){
+        const key = localStoragekeys[i]
+        const userdetailsStrings= localStorageobj[key];
+        const userdeailsObj = JSON.parse(userdetailsStrings);
+        ShowNewUser(userdeailsObj)
+    }
+});
+
+//Adding new user details
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    
+    let myobj={
+        name:namebox.value,
+        email:emailbox.value,
+    };
+
+    let myobj_serialized=JSON.stringify(myobj);
+    localStorage.setItem(myobj.email,myobj_serialized);
+    // let myobj_deserialized=JSON.parse(localStorage.getItem(myobj.email));
+    // console.log(myobj_deserialized)
+    ShowNewUser(myobj) 
+})
+
+function ShowNewUser(userdetails){
+    document.getElementById('email').value='';
+    document.getElementById('name').value='';
+
+    if(localStorage.getItem(userdetails.email)!== null){
+        //console.log(userdetails.email)
+        removeUserFromScreen(userdetails.email)
+    }
+
+    let MainNodeVariable=document.getElementById('itemss');
+    let childHtml=`<li id=${userdetails.email}> 
+                        <b>Name:</b> ${userdetails.name} -<b> Email:</b> ${userdetails.email} 
+                        <button onclick=deleteser('${userdetails.email}' class="buttonDly") > Remove </button> 
+                        <button onclick=removeuser('${userdetails.email}','${userdetails.name}' class="buttonEdt")> edit </button> 
+                    </li> `;
+    MainNodeVariable.innerHTML=MainNodeVariable.innerHTML + childHtml;
+}
+
+//remove details from the localstorage
+function deleteser(userid){
+    console.log(userid);
+    localStorage.removeItem(userid);
+    removeUserFromScreen(userid);
+}
+
+//remove user from the frontend
+function removeUserFromScreen(userid){
+    const parentNode=document.getElementById('itemss');
+    const childNodeToBeDeleted = document.getElementById(userid);
+    if(childNodeToBeDeleted){
+        parentNode.removeChild(childNodeToBeDeleted);
+    }
+}
+
+//edit user details
+function removeuser(useremail, username){
+    let Namebox=document.getElementById('name');
+    let EmailBox=document.getElementById('email');
+
+    Namebox.value = username;
+    EmailBox.value = useremail;
+    deleteser(useremail)
+}
